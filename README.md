db9366cccd772aff688852a5e3b64b2373882ce5
Marat Prokofev <maratprok@yandex.ru>
2026-02-15 20:58:31 +0300

    creating news list adn fix card

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ git remote add origin https://github.com/Hel1pls/test_500na700
 
 creating a
 git add .
-git commit -m "creating api and news card"
+git commit -m "creating news list adn fix card"
 git push 
 
 
--- src/entities/news/ui/NewsCard.tsx
+++ src/entities/news/ui/NewsCard.tsx
@@ -14,15 +14,21 @@ export const NewsCard: React.FC<NewsCardProps> = ({
 	date,
 	image,
 }) => {
+	// Максимальная длина описания (примерно как в json)
+	const MAX_DESC = 125
+	const shortDescription =
+		description.length > MAX_DESC
+			? description.slice(0, MAX_DESC).trim() + '...'
+			: description
+
 	return (
 		<div className={styles.card}>
-			<div
-				className={styles.image}
-				style={{ backgroundImage: `url(${image})` }}
-			/>
+			<div className={styles.image}>
+				<img src={image} alt={title} />
+			</div>
 			<div className={styles.content}>
 				<h2 className={styles.title}>{title}</h2>
-				<div className={styles.description}>{description}</div>
+				<div className={styles.description}>{shortDescription}</div>
 			</div>
 			<div className={styles.date}>{date}</div>
 		</div>
